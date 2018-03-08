2f975b40e834d49853586b1bb9935d5edf3ad6ed
Michael Hiebl <hieblmi@gmail.com>
2018-03-07 21:02:29 -0500

    Added SudokuActivity

--- app/src/main/java/com/hieblmi/sudoku/MainActivity.java
+++ app/src/main/java/com/hieblmi/sudoku/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hieblmi.sudoku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startSudokuActivity(View view) {
+        Intent intent = new Intent(this, SudokuActivity.class);
+        startActivity(intent);
     }
 }
